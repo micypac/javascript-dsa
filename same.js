a7d9aee565a29fdda15541c0269c3f12064fc007
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-04 21:26:20 -0400

    Refactored same.js to improve time complexity.

--- same.js
+++ same.js
@@ -7,20 +7,55 @@
     has its corresponding squared value in the second array. The frequency of values must be the same. Order mix up is ok.
 */
 
-// Naive solution using O(n^2)
+//***  Naive solution using O(n^2)
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false
+//   }
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2)
+
+//     if (correctIndex === -1) {
+//       return false
+//     }
+//     arr2.splice(correctIndex, 1)
+//   }
+//   return true
+// }
+
+// Efficient solution
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false
   }
 
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2)
+  const frequencyCounter1 = {}
+  const frequencyCounter2 = {}
 
-    if (correctIndex === -1) {
+  for (const item of arr1) {
+    frequencyCounter1[item] = (frequencyCounter1[item] || 0) + 1 // check if key exist, if not initialized to 0. // if key exist, increment value by 1.
+  }
+
+  for (const item of arr2) {
+    frequencyCounter2[item] = (frequencyCounter2[item] || 0) + 1
+  }
+
+  console.log(frequencyCounter1)
+  console.log(frequencyCounter2)
+
+  for (const key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      // check if key in obj1 exist as key**2 in obj2.
       return false
     }
-    arr2.splice(correctIndex, 1)
+
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      // check if frequency values are the same for both objects.
+      return false
+    }
   }
+
   return true
 }
 
