d9c01ee5092d1ebfd2989d1cd84ad33afd0b8a68
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-19 10:24:51 -0400

    implemented exer#14 fib(Fibonacci) function using recursion.

---
+++ Recursion/fib.js
@@ -0,0 +1,20 @@
+/*
+  Write a recursive function called fib which accepts a number and 
+  returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is 
+  the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, 
+  and where every number thereafter is equal to the sum of the previous two numbers.
+
+  fib(4) // 3
+  fib(10) // 55
+  fib(28) // 317811
+  fib(35) // 9227465
+*/
+
+const fib = (num) => {
+  if (num === 0) return 0
+  if (num === 1) return 1
+
+  return fib(num - 2) + fib(num - 1)
+}
+
+console.log(fib(5))
