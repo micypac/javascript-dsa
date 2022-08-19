ab55134fa12f3f94ee9c836798764e9629cebad8
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-18 23:20:47 -0400

    implemented exer#12 productOfArray function using recursion.

---
+++ Recursion/productOfArray.js
@@ -0,0 +1,15 @@
+/*
+  Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+  productOfArray([1,2,3]) // 6
+  productOfArray([1,2,3,10]) // 60
+*/
+
+const productOfArray = (arr) => {
+  if (!arr.length) return 0
+  if (arr.length === 1) return arr[0]
+
+  return arr[0] * productOfArray(arr.slice(1))
+}
+
+console.log(productOfArray([1, 2, 3, 10]))
