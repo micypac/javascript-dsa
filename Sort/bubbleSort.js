29c49f67592be4d5e400171ff8faadf7584284a8
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-22 19:29:43 -0400

    added bubbleSort algorithm.

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,25 @@
+const bubbleSort = (arr) => {
+  // if array is almost sorted, this additional optimization will remove redundancy of looping.
+  let noSwaps // variable to track if no swap happened to short-circuit and cut-off unnecessary iteration.
+
+  // outer loop will start at the right end because each inner loop completion will push the largest value at this end.
+  // next iteration, rightmost index shouldn't be compared for swapping.
+  for (let i = arr.length - 1; i >= 0; i--) {
+    noSwaps = true
+
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+        noSwaps = false
+      }
+    }
+
+    if (noSwaps) break
+  }
+
+  return arr
+}
+
+console.log(bubbleSort([5, 3, 6, 4, 7, 2, 1, 8]))
