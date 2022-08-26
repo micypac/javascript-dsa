209ee5fbccf9e313bbfacef584c494bf8f9f41a7
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-25 23:59:32 -0400

    improved isPrime implementation.

--- Linear/isPrime.js
+++ Linear/isPrime.js
@@ -3,7 +3,12 @@ const isPrime = (n) => {
     return false
   }
 
-  for (let i = 2; i < n; i++) {
+  /*** Optimized primality test
+    Integers larger than the square root do not need to be checked because, whenever n=a*b,
+    one of the two factors a and b is less than or equal to the square root of n
+  */
+  // for (let i = 2; i < n; i++) {
+  for (let i = 2; i < Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false
     }
