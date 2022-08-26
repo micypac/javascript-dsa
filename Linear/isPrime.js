e4c63d1291908dbc9629cb4572ed932caeec3f21
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-25 23:43:25 -0400

    added isPrime implementation.

---
+++ Linear/isPrime.js
@@ -0,0 +1,17 @@
+const isPrime = (n) => {
+  if (n < 2) {
+    return false
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(isPrime(1))
+console.log(isPrime(6))
+console.log(isPrime(11))
