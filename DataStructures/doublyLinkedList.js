df869dacad16deef348e3ed283797a24c0c4b179
Michael Pacleb <michael.pacleb@gmail.com>
2022-08-31 13:26:47 -0400

    implemented push method for doublyLinkedList.

--- DataStructures/doublyLinkedList.js
+++ DataStructures/doublyLinkedList.js
@@ -12,4 +12,30 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+
+  /*
+    push pseudocode
+    - create a new node using the value passed to the function
+    - if list is empty, set the new node as the head and tail property of the list
+    - otherwise, set the next property of the tail to be the new node
+    - set the prev property of the new node to be the tail
+    - and set the tail list property as the new node
+    - increment list
+    - return list
+  */
+  push(val) {
+    let newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+
+    this.length++
+    return this
+  }
 }
