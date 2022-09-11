31929f79c90c892988329d8e4586d4f246284521
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-11 17:59:30 -0400

    implemented graph with addVertex and addEdge methods.

---
+++ DataStructures/graph.js
@@ -0,0 +1,32 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+  }
+
+  /*
+    adding an edge
+    - this function should accept 2 vertices
+    - the function should find in the adjacency list the key of vertex1 and push vertex2 to the array
+    - the function should find in the adjacency list the key of vertex2 and push vertex1 to the array
+    - set aside handling errors or invalid vertices for now
+  */
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2)
+    this.adjacencyList[v2].push(v1)
+  }
+}
+
+const flightGraph = new Graph()
+
+flightGraph.addVertex('Newark')
+flightGraph.addVertex('Ontario')
+flightGraph.addVertex('Dallas')
+
+flightGraph.addEdge('Newark', 'Ontario')
+flightGraph.addEdge('Newark', 'Dallas')
+
+console.log(flightGraph)
