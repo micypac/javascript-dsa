71209b853b15931594a540784476c962266bef76
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-11 18:11:37 -0400

    implemented removeEdge method for graph.

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -18,6 +18,18 @@ class Graph {
     this.adjacencyList[v1].push(v2)
     this.adjacencyList[v2].push(v1)
   }
+
+  /*
+    removing an edge
+    - this function should accept 2 vertices
+    - the function should reassign the key of vertex1 to be an array that does not contain vertex2
+    - the function should reassign the key of vertex2 to be an array that does not contain vertex1
+    - set aside handling errors or invalid vertices for now
+  */
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1].splice(this.adjacencyList[v1].indexOf(v2), 1)
+    this.adjacencyList[v2].splice(this.adjacencyList[v2].indexOf(v1), 1)
+  }
 }
 
 const flightGraph = new Graph()
@@ -29,4 +41,7 @@ flightGraph.addVertex('Dallas')
 flightGraph.addEdge('Newark', 'Ontario')
 flightGraph.addEdge('Newark', 'Dallas')
 
+console.log(flightGraph)
+flightGraph.removeEdge('Newark', 'Dallas')
+flightGraph.removeEdge('Newark', 'Ontario')
 console.log(flightGraph)
