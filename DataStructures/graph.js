01902fad2f5ac2f61c465ac79ca86e8fcc19a44b
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-11 18:51:38 -0400

    implemented removeVertex method for graph.

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -27,21 +27,48 @@ class Graph {
     - set aside handling errors or invalid vertices for now
   */
   removeEdge(v1, v2) {
-    this.adjacencyList[v1].splice(this.adjacencyList[v1].indexOf(v2), 1)
-    this.adjacencyList[v2].splice(this.adjacencyList[v2].indexOf(v1), 1)
+    //*** course solution */
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+      (item) => item !== v2,
+    )
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+      (item) => item !== v1,
+    )
+  }
+
+  /*
+    removing a vertex
+    - the function should accept a vertex to remove
+    - the function should loop as long there are any other vertices in the adjacency list for that vertex
+    - inside the loop, call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+    - delete the key in the adjacency list for that vertex
+  */
+  removeVertex(v) {
+    for (const item of this.adjacencyList[v]) {
+      this.removeEdge(item, v)
+    }
+
+    delete this.adjacencyList[v]
   }
 }
 
 const flightGraph = new Graph()
 
-flightGraph.addVertex('Newark')
-flightGraph.addVertex('Ontario')
+flightGraph.addVertex('Tokyo')
 flightGraph.addVertex('Dallas')
+flightGraph.addVertex('Aspen')
+flightGraph.addVertex('Hong Kong')
+flightGraph.addVertex('Los Angeles')
 
-flightGraph.addEdge('Newark', 'Ontario')
-flightGraph.addEdge('Newark', 'Dallas')
-
+flightGraph.addEdge('Tokyo', 'Dallas')
+flightGraph.addEdge('Tokyo', 'Hong Kong')
+flightGraph.addEdge('Dallas', 'Aspen')
+flightGraph.addEdge('Dallas', 'Hong Kong')
+flightGraph.addEdge('Dallas', 'Los Angeles')
+flightGraph.addEdge('Los Angeles', 'Hong Kong')
 console.log(flightGraph)
-flightGraph.removeEdge('Newark', 'Dallas')
-flightGraph.removeEdge('Newark', 'Ontario')
+// console.log(flightGraph)
+// flightGraph.removeEdge('Newark', 'Dallas')
+// flightGraph.removeEdge('Newark', 'Ontario')
+flightGraph.removeVertex('Hong Kong')
 console.log(flightGraph)
