42fd2e179b46aa1b465925bafda088800f18aa9e
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-11 23:02:21 -0400

    implemented DFSR(recursive) method for graph.

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -50,25 +50,75 @@ class Graph {
 
     delete this.adjacencyList[v]
   }
+
+  /*
+    DFS (recursive)
+    - function should accept starting vertex/node to traverse (like root for BST)
+    - create a list to store the end result and return at end
+    - create an object to store visited nodes
+    - create a helper function which accepts a node/vertex
+      - if node/vertex is empty, return (this is the base case)
+      - add vertex to result list and mark them as visited
+      - loop over all the values in the adjacencyList for that node/vertex
+      - if any of the values is not visited, recursively call the helper function with that node/vertex
+  */
+  DFSR(start) {
+    if (!this.adjacencyList[start]) return null
+    const result = []
+    const visited = {}
+    // preserving the graph since the "this" inside helper function has now different context
+    const adjacencyList = this.adjacencyList
+
+    const helper = (node) => {
+      result.push(node)
+      visited[node] = true
+      adjacencyList[node].forEach((element) => {
+        if (!visited[element]) {
+          return helper(element)
+        }
+      })
+    }
+
+    helper(start)
+    return result
+  }
 }
 
 const flightGraph = new Graph()
 
-flightGraph.addVertex('Tokyo')
-flightGraph.addVertex('Dallas')
-flightGraph.addVertex('Aspen')
-flightGraph.addVertex('Hong Kong')
-flightGraph.addVertex('Los Angeles')
+// flightGraph.addVertex('Tokyo')
+// flightGraph.addVertex('Dallas')
+// flightGraph.addVertex('Aspen')
+// flightGraph.addVertex('Hong Kong')
+// flightGraph.addVertex('Los Angeles')
 
-flightGraph.addEdge('Tokyo', 'Dallas')
-flightGraph.addEdge('Tokyo', 'Hong Kong')
-flightGraph.addEdge('Dallas', 'Aspen')
-flightGraph.addEdge('Dallas', 'Hong Kong')
-flightGraph.addEdge('Dallas', 'Los Angeles')
-flightGraph.addEdge('Los Angeles', 'Hong Kong')
-console.log(flightGraph)
+// flightGraph.addEdge('Tokyo', 'Dallas')
+// flightGraph.addEdge('Tokyo', 'Hong Kong')
+// flightGraph.addEdge('Dallas', 'Aspen')
+// flightGraph.addEdge('Dallas', 'Hong Kong')
+// flightGraph.addEdge('Dallas', 'Los Angeles')
+// flightGraph.addEdge('Los Angeles', 'Hong Kong')
+// console.log(flightGraph)
 // console.log(flightGraph)
 // flightGraph.removeEdge('Newark', 'Dallas')
 // flightGraph.removeEdge('Newark', 'Ontario')
-flightGraph.removeVertex('Hong Kong')
+// flightGraph.removeVertex('Hong Kong')
+// console.log(flightGraph)
+
+flightGraph.addVertex('A')
+flightGraph.addVertex('B')
+flightGraph.addVertex('C')
+flightGraph.addVertex('D')
+flightGraph.addVertex('E')
+flightGraph.addVertex('F')
+
+flightGraph.addEdge('A', 'B')
+flightGraph.addEdge('A', 'C')
+flightGraph.addEdge('B', 'D')
+flightGraph.addEdge('C', 'E')
+flightGraph.addEdge('D', 'E')
+flightGraph.addEdge('D', 'F')
+flightGraph.addEdge('E', 'F')
+
 console.log(flightGraph)
+console.log(flightGraph.DFSR('M'))
