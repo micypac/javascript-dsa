e3f7d1853580b7d2c0fbf4aba8ebf823defd5a2a
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-20 19:28:54 -0400

    implemented breadth first search method for binaryTree.

--- FCC/binaryTree.js
+++ FCC/binaryTree.js
@@ -53,5 +53,24 @@ const depthFirstSearchR = (root) => {
   return [root.val, ...left, ...right]
 }
 
+// Breadth first search.
+const breadthFirstSearchI = (root) => {
+  if (root === null) return []
+
+  const result = []
+  const queue = [root]
+
+  while (queue.length > 0) {
+    const current = queue.shift()
+    result.push(current.val)
+
+    if (current.left) queue.push(current.left)
+    if (current.right) queue.push(current.right)
+  }
+
+  return result
+}
+
 console.log(depthFirstSearchI(a))
 console.log(depthFirstSearchR(a))
+console.log(breadthFirstSearchI(a))
