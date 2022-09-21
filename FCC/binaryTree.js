5aec5c2ffbecab4cb5e32170d0011a0a3e5839b7
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-20 20:12:38 -0400

    implemented treeSum methods for binaryTree.

--- FCC/binaryTree.js
+++ FCC/binaryTree.js
@@ -6,27 +6,6 @@ class Node {
   }
 }
 
-const a = new Node('a')
-const b = new Node('b')
-const c = new Node('c')
-const d = new Node('d')
-const e = new Node('e')
-const f = new Node('f')
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
-
-/*
-        a
-      /   \
-     b     c
-    / \.    \
-   d.  e.    f
-*/
-
 // Depth first search iterative.
 const depthFirstSearchI = (root) => {
   if (root === null) return []
@@ -95,9 +74,67 @@ const treeIncludesR = (root, target) => {
   return treeIncludesR(root.left, target) || treeIncludesR(root.right, target)
 }
 
+// Tree sum. Return the sum of values for the entire tree. Using recursive.
+const treeSumR = (root) => {
+  if (root === null) return 0
+  return root.val + treeSumR(root.left) + treeSumR(root.right)
+}
+
+// Tree sum. Return the sum of values for the entire tree. Using iterative.
+const treeSumI = (root) => {
+  if (root === null) return 0
+  let sum = 0
+  const queue = [root]
+  while (queue.length > 0) {
+    const current = queue.shift()
+    sum += current.val
+
+    if (current.left) queue.push(current.left)
+    if (current.right) queue.push(current.right)
+  }
+
+  return sum
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+/*
+        a
+      /   \
+     b     c
+    / \.    \
+   d.  e.    f
+*/
+
+const a1 = new Node(5)
+const b1 = new Node(10)
+const c1 = new Node(6)
+const d1 = new Node(4)
+const e1 = new Node(12)
+const f1 = new Node(1)
+
+a1.left = b1
+a1.right = c1
+b1.left = d1
+b1.right = e1
+c1.right = f1
+
 console.log(depthFirstSearchI(a))
 console.log(depthFirstSearchR(a))
 console.log(breadthFirstSearchI(a))
 console.log(treeIncludesI(a, 'e'))
 console.log(treeIncludesI(a, 'm'))
 console.log(treeIncludesR(a, 'e'))
+console.log(treeSumR(a1))
+console.log(treeSumI(a1))
