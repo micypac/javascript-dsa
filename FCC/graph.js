25feb2f58f0a75e43885185fd9ea6c3dbb873085
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-21 15:07:48 -0400

    Implemented hasPath methods both recursively and iteratively for graph.

--- FCC/graph.js
+++ FCC/graph.js
@@ -56,6 +56,60 @@ const breadthFirstPrint = (graph, source) => {
   }
 }
 
-depthFirstPrintI(graph, 'a')
-depthFirstPrintR(graph, 'a')
-breadthFirstPrint(graph, 'a')
+// depthFirstPrintI(graph, 'a')
+// depthFirstPrintR(graph, 'a')
+// breadthFirstPrint(graph, 'a')
+
+const graph2 = {
+  f: ['g', 'i'],
+  g: ['h'],
+  h: [],
+  i: ['g', 'k'],
+  j: ['i'],
+  k: [],
+}
+
+/* Directed unweighted graph
+      f  → g  → h    
+      ↓  ↗   
+      i  ← j
+      ↓
+      k  
+*/
+
+/*****************************************************
+ * HAS PATH
+ * Define a function that takes an object representing the adjacency list of a directed acyclic graph and two nodes (src, and dst).
+ * The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
+ *****************************************************/
+
+// Recursive
+const hasPathR = (graph, src, dst) => {
+  if (src === dst) return true
+
+  for (let neighbor of graph[src]) {
+    if (hasPathR(graph, neighbor, dst) === true) return true
+  }
+
+  return false
+}
+
+// Iterative using BFS
+const hasPathI = (graph, src, dst) => {
+  const queue = [src]
+
+  while (queue.length > 0) {
+    const current = queue.shift()
+
+    if (current === dst) return true
+
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor)
+    }
+  }
+
+  return false
+}
+
+console.log(hasPathR(graph2, 'f', 'j'))
+console.log(hasPathI(graph2, 'f', 'k'))
