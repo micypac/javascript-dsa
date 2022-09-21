ab37320c9599f63fd478c17a6afa16e8c239e8ba
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-21 16:35:37 -0400

    Implemented connectedComponents method for graph.

--- FCC/graph.js
+++ FCC/graph.js
@@ -180,3 +180,54 @@ const hasUndirectedPath = (graph, src, dst, visited) => {
 
 console.log(undirectedPath(graph3, 'i', 'm'))
 console.log(undirectedPath(graph3, 'i', 'n'))
+
+/*****************************************************
+ * CONNECTED COMPONENTS
+ * Define a function that takes in the adjacency list of an undirected graph.
+ * The function should return the number of components within the graph.
+ *****************************************************/
+
+const connectedComponents = (graph) => {
+  const visited = new Set()
+  let count = 0
+
+  for (let node in graph) {
+    if (explore(graph, node, visited) === true) count += 1
+  }
+
+  return count
+}
+
+const explore = (graph, current, visited) => {
+  if (visited.has(String(current))) return false
+
+  visited.add(String(current))
+
+  for (let neighbor of graph[current]) {
+    explore(graph, neighbor, visited)
+  }
+
+  return true
+}
+
+const graph4 = {
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+}
+
+/* 
+          5
+          |  \
+    1 --- 0 --- 8
+      
+    4 --- 2
+       \  |
+          3
+*/
+
+console.log(connectedComponents(graph4)) // 2
