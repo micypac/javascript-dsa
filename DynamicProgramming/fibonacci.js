aafb04c2ee7f166e152e74938c1943fd29d28c74
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-22 19:57:07 -0400

    Implemented fibonacci function using recursive and dynamic programming approach.

---
+++ DynamicProgramming/fibonacci.js
@@ -0,0 +1,20 @@
+// Recursive approach
+// Time complexity O(2^n) Space complexity O(n)
+const fibonacci = (n) => {
+  if (n <= 2) return 1
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(fibonacci(7))
+console.log(fibonacci(50)) // don't run this, it will take a long time!!!
+
+// Using memoization
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n]
+  if (n <= 2) return 1
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+  return memo[n]
+}
+
+console.log(fib(50))
