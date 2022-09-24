d10b0c652e650db54a5771dafc0261c8fa237585
Michael Pacleb <michael.pacleb@gmail.com>
2022-09-24 17:01:30 -0400

    Implemented countConstruct problem using recursion and memoization approach.

--- DynamicProgramming/countConstruct.js
+++ DynamicProgramming/countConstruct.js
@@ -7,3 +7,95 @@
   countConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']) => 0
   countConstruct('purple',['purp','p','ur','le','purpl']) => 2
 */
+
+/*****************************************************
+ * Recursive
+ * m = target.length
+ * n = array word bank length
+ * Time O((n^m) * m); Space O(m^2)
+ *****************************************************/
+const countConstruct = (target, wordBank) => {
+  if (target.length === 0) return 1
+
+  let totalCount = 0
+  for (let word of wordBank) {
+    if (target.startsWith(word)) {
+      const suffix = target.slice(word.length)
+
+      const count = countConstruct(suffix, wordBank)
+
+      totalCount += count
+    }
+  }
+
+  return totalCount
+}
+
+console.log(countConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) // 1
+console.log(countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl'])) // 2
+console.log(
+  countConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']),
+) // 0
+console.log(
+  countConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']),
+) // 4
+// console.log(
+//   countConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+//     'e',
+//     'ee',
+//     'eee',
+//     'eeee',
+//     'eeeee',
+//     'eeeeee',
+//   ]),
+// ) // false. It runs a while!!!
+
+/*****************************************************
+ * Memoized version
+ * Time O(n * m^2); Space O(m^2)
+ *****************************************************/
+const countConstruct2 = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target]
+  if (target.length === 0) return 1
+
+  let totalCount = 0
+
+  for (let word of wordBank) {
+    if (target.startsWith(word)) {
+      const suffix = target.slice(word.length)
+      const count = countConstruct2(suffix, wordBank, memo)
+      totalCount += count
+    }
+  }
+
+  memo[target] = totalCount
+  return totalCount
+}
+
+console.log('Memoize Run!!!')
+console.log(countConstruct2('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) // 1
+console.log(countConstruct2('purple', ['purp', 'p', 'ur', 'le', 'purpl'])) // 2
+console.log(
+  countConstruct2('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']),
+) // 0
+console.log(
+  countConstruct2('enterapotentpot', [
+    'a',
+    'p',
+    'ent',
+    'enter',
+    'ot',
+    'o',
+    't',
+  ]),
+) // 4
+console.log(
+  countConstruct2('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeee',
+  ]),
+) // false. It runs a while!!!
