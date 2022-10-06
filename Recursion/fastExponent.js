fb256beb2de3e0ad4c6062b034da383dbdbbcf56
Michael Pacleb <michael.pacleb@gmail.com>
2022-10-06 04:17:07 -0400

    implemented fastExponent using recursion.

---
+++ Recursion/fastExponent.js
@@ -0,0 +1,63 @@
+/***********************************************************************
+Write a more advanced version of the recursive `exponent` function. 
+Instead of multiplying the base number by itself n power of
+times, you will be squaring the results of the base
+number raised to the power of half of n power.
+
+The following is math, not JavaScript:
+
+exponent(b, 0) // 1
+exponent(b, 1) // b
+exponent(b, n) // exponent(b, n / 2) ** 2             [for even n]
+exponent(b, n) // b * (exponent(b, (n - 1) / 2) ** 2) [for odd n]
+
+You will need to square the results of 
+exponent(b, n / 2) and (exponent(b, (n - 1) / 2).
+
+Examples:
+
+advancedExponent(2, 0); // 1
+advancedExponent(2, 1); // 2
+advancedExponent(2, 2); // 4
+advancedExponent(2, 3); // 8
+advancedExponent(2, 4); // 16
+advancedExponent(2, 5); // 32
+advancedExponent(2, 6); // 64
+advancedExponent(2, 7); // 128
+advancedExponent(2, 8); // 256
+advancedExponent(2, 9); // 512
+advancedExponent(2, 10); // 1024
+advancedExponent(2, 11); // 2048
+advancedExponent(2, 12); // 4096
+
+For each of the examples above, figure out how many times your code should
+be recursively calling `fastExponent`. Find a way to visually see how many
+times `fastExponent` is being recursively called.
+***********************************************************************/
+
+// function advancedExponent(b, n) {
+//   const helper = (b, n) => {
+//     if (n === 0) return 1;
+//     return b * helper(b, n - 1);
+//   };
+
+//   if (n % 2 === 0) return helper(b, n / 2) ** 2;
+//   else if (n % 2 === 1) return b * helper(b, (n - 1) / 2) ** 2;
+// }
+
+const fastExponent = (b, n) => {
+  if (n === 0) return 1;
+  else if (n % 2 === 0) return fastExponent(b * b, n / 2);
+  else return b * fastExponent(b * b, (n - 1) / 2);
+};
+
+console.log(fastExponent(2, 3)); // 8
+console.log(fastExponent(2, 4)); // 16
+console.log(fastExponent(2, 5)); // 32
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = advancedExponent;
+} catch (e) {
+  module.exports = null;
+}
